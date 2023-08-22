05499f35413b09a38934b5cda071e42499fd4774
Morning-Shine <maskacat@gmail.com>
2023-08-22 15:36:42 +0300

    Испр. ошибка стилей при исп-и динамических имён классов

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,8 @@ import './App.css';
 const App: React.FC = () => {
   return (
     <div
-      // className={`bg-neutral-50 dark:bg-neutral-800 h-screen`}
-      // className={`bg-pink-500 dark:bg-${COLOR_CONTRAST}-800 h-full min-h-screen`}
-      className={`bg-${COLOR_CONTRAST}-50 dark:bg-${COLOR_CONTRAST}-800 h-full min-h-screen
-       flex flex-col`}
+      className={`bg-${COLOR_CONTRAST}-50 dark:bg-${COLOR_CONTRAST}-800
+                  h-full min-h-screen flex flex-col`}
     >
       <Header />
       <AppRoutes />
--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -2,16 +2,17 @@ import React, { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { TModalProps } from './type';
 import { IoMdClose } from 'react-icons/io';
+import classNames from 'classnames';
 
 const Modal: React.FC<TModalProps> = (props) => {
   const { children, onClose, size } = props;
   let top;
   switch (size) {
     case 'sm':
-      top = '56';
+      top = 'top-56';
       break;
     case 'md':
-      top = '24';
+      top = 'top-24';
       break;
   }
 
@@ -42,7 +43,10 @@ const Modal: React.FC<TModalProps> = (props) => {
       onClick={(e) => onCloseHandler(e)}
     >
       <div
-        className={`h-100 w-min bg-white dark:bg-black bg-opacity-100 mx-auto p-5 rounded-md relative top-${top}`}
+        className={classNames(
+          'h-100 w-min bg-white dark:bg-black bg-opacity-100 mx-auto p-5 rounded-md relative',
+          top
+        )}
       >
         <div className="absolute top-2 right-2 text-black dark:text-white hover:text-red-600 hover:dark:text-red-800">
           <IoMdClose
